79e078c9f4b521141a7fe1424262768e26e80278
Eva Haring <eva.haring@student.tugraz.at>
2025-11-15 10:35:01 +0100

    Different Masonry layout for different screen sizes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,52 +1,128 @@
 <script lang="ts">
 	import { articles } from '$lib/placeholder/articles';
 	import Article from '$lib/components/Article.svelte';
+	import { SunMoon } from '@lucide/svelte';
+
+	let lightTheme = true;
+
+	const toggleTheme = () => {
+		lightTheme = !lightTheme;
+	}
+
 </script>
 
-<div class="news-page">
-	{#each articles as article (article.title)}
-		<div class="article-card"
-				 class:main-article={article.flag === 'main'}
-				 class:medium-article={article.flag === 'medium'}>
-			<Article
-				title={article.title}
-				content={article.content}
-				image={article.img}
-			/>
-		</div>
-	{/each}
+<div class="main-content">
+	<div class="theme-settings">
+		<button
+			class="theme-settings__button"
+			on:click={toggleTheme}
+			aria-label="Toggle theme"
+		>
+			<SunMoon />
+		</button>
+	</div>
+
+	<div class="article-layout">
+		{#each articles as article (article.title)}
+			<div class="article-card"
+					 class:main-article={article.flag === 'main'}
+					 class:medium-article={article.flag === 'medium'}>
+				<Article
+					title={article.title}
+					content={article.content}
+					image={article.img}
+				/>
+			</div>
+		{/each}
+	</div>
 </div>
 
 <style>
-    .news-page {
+.main-content {
+    display: flex;
+    flex-direction: column;
+    container-type: inline-size;
+    width: 90%;
+    margin: auto;
+
+		.theme-settings {
+				display: flex;
+				flex-direction: column;
+				align-items: flex-end;
+				justify-content: center;
+
+				.theme-settings__button {
+					display: flex;
+					margin: auto 0;
+					background: none;
+					border: none;
+					cursor: pointer;
+				}
+		}
+
+    .article-layout {
 				padding: 1rem;
-				max-width: 80rem;
-        display: masonry;
 				margin: 0 auto;
+
+        display: masonry;
         grid-template-columns: repeat(4, 1fr);
-				grid-template-rows: masonry;
+        grid-template-rows: masonry;
+
+        .article-card {
+            padding: 1rem;
+            margin: 1rem;
+            background: #FAFAFA;
+            border-radius: 1rem;
+            transition: all 0.2s ease-in-out;
+
+            &:hover {
+                transform: scale(1.01);
+            }
+
+            &.main-article {
+                grid-column: span 3;
+            }
+
+            &.medium-article {
+                grid-column: span 2;
+            }
+        }
     }
 
-		.article-card {
-				padding: 1rem;
-				margin: 1rem;
-				background: #FAFAFA;
-				border-radius: 1rem;
-        transition: all 0.2s ease-in-out;
-        box-shadow: 0 1em 1em rgba(0, 0, 0, 0.1);
 
-				&:hover {
-            box-shadow: 0 1em 1em rgba(0, 0, 0, 0.1);
-            transform: scale(1.01);
+    @container (max-width: 1104px) {
+        .article-layout{
+            grid-template-columns: repeat(2, 1fr);
+            grid-template-rows: masonry;
         }
 
-				&.main-article {
-            grid-column: span 3;
+        .article-layout .article-card {
+            &.main-article {
+                grid-column: span 2;
+            }
+
+            &.medium-article {
+                grid-column: span 1;
+            }
+        }
+    }
+
+    @container (max-width: 512px) {
+        .article-layout{
+            grid-template-columns: repeat(1, 1fr);
+            grid-template-rows: masonry;
         }
 
-        &.medium-article {
-            grid-column: span 2;
+        .article-layout .article-card {
+            &.main-article {
+                grid-column: span 1;
+            }
+
+            &.medium-article {
+                grid-column: span 1;
+            }
         }
-		}
+    }
+}
 
 </style>
