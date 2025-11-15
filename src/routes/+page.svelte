9853857fc4da0b272603bb8db19d508408a1e070
Eva Haring <eva.haring@student.tugraz.at>
2025-11-15 11:04:13 +0100

    Used @supports for masonry, fallback normal grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,122 +7,122 @@
 
 	const toggleTheme = () => {
 		lightTheme = !lightTheme;
-	}
-
+	};
 </script>
 
 <div class="main-content">
 	<div class="theme-settings">
-		<button
-			class="theme-settings__button"
-			on:click={toggleTheme}
-			aria-label="Toggle theme"
-		>
+		<button class="theme-settings__button" on:click={toggleTheme} aria-label="Toggle theme">
 			<SunMoon />
 		</button>
 	</div>
 
 	<div class="article-layout">
 		{#each articles as article (article.title)}
-			<div class="article-card"
-					 class:main-article={article.flag === 'main'}
-					 class:medium-article={article.flag === 'medium'}>
-				<Article
-					title={article.title}
-					content={article.content}
-					image={article.img}
-				/>
+			<div
+				class="article-card"
+				class:main-article={article.flag === 'main'}
+				class:medium-article={article.flag === 'medium'}
+			>
+				<Article title={article.title} content={article.content} image={article.img} />
 			</div>
 		{/each}
 	</div>
 </div>
 
 <style>
-.main-content {
-    display: flex;
-    flex-direction: column;
-    container-type: inline-size;
-    width: 90%;
-    margin: auto;
+	:root {
+		color-scheme: light;
+	}
+
+	.main-content {
+		display: flex;
+		flex-direction: column;
+		container-type: inline-size;
+		width: 90%;
+		margin: auto;
 
 		.theme-settings {
-				display: flex;
-				flex-direction: column;
-				align-items: flex-end;
-				justify-content: center;
+			display: flex;
+			flex-direction: column;
+			align-items: flex-end;
+			justify-content: center;
 
-				.theme-settings__button {
-					display: flex;
-					margin: auto 0;
-					background: none;
-					border: none;
-					cursor: pointer;
-				}
+			.theme-settings__button {
+				display: flex;
+				margin: auto 0;
+				background: none;
+				border: none;
+				cursor: pointer;
+			}
 		}
 
     .article-layout {
-				padding: 1rem;
-				margin: 0 auto;
+      padding: 1rem;
+      margin: 0 auto;
 
-        display: masonry;
-        grid-template-columns: repeat(4, 1fr);
-        grid-template-rows: masonry;
-
-        .article-card {
-            padding: 1rem;
-            margin: 1rem;
-            background: #FAFAFA;
-            border-radius: 1rem;
-            transition: all 0.2s ease-in-out;
-
-            &:hover {
-                transform: scale(1.01);
-            }
-
-            &.main-article {
-                grid-column: span 3;
-            }
-
-            &.medium-article {
-                grid-column: span 2;
-            }
-        }
+      /* fallback for masonry */
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
     }
 
+    .article-card {
+      padding: 1rem;
+      margin: 1rem;
+      background: #fafafa;
+      border-radius: 1rem;
+      transition: all 0.2s ease-in-out;
+
+      &:hover {
+        transform: scale(1.01);
+      }
+
+      &.main-article {
+        grid-column: span 3;
+      }
+
+      &.medium-article {
+        grid-column: span 2;
+      }
+    }
+
+		@supports (display: masonry) {
+			.article-layout {
+        display: masonry;
+				grid-template-rows: masonry;
+			}
+		}
 
     @container (max-width: 1104px) {
-        .article-layout{
-            grid-template-columns: repeat(2, 1fr);
-            grid-template-rows: masonry;
+      .article-layout {
+        grid-template-columns: repeat(2, 1fr);
+      }
+
+      .article-layout .article-card {
+        &.main-article {
+          grid-column: span 2;
         }
 
-        .article-layout .article-card {
-            &.main-article {
-                grid-column: span 2;
-            }
-
-            &.medium-article {
-                grid-column: span 1;
-            }
+        &.medium-article {
+          grid-column: span 1;
         }
+      }
     }
 
     @container (max-width: 512px) {
-        .article-layout{
-            grid-template-columns: repeat(1, 1fr);
-            grid-template-rows: masonry;
+      .article-layout {
+        grid-template-columns: repeat(1, 1fr);
+      }
+
+      .article-layout .article-card {
+        &.main-article {
+          grid-column: span 1;
         }
 
-        .article-layout .article-card {
-            &.main-article {
-                grid-column: span 1;
-            }
-
-            &.medium-article {
-                grid-column: span 1;
-            }
+        &.medium-article {
+          grid-column: span 1;
         }
+      }
     }
-}
-
+	}
 </style>
